615445b3d1950d6d05a51162c9cb70cd6aed4e4b
ZeeKotomori <zikriaf23@gmail.com>
2025-02-08 10:48:44 +0700

    feat : math funct

---
+++ math.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+
+addition = a + b
+print(f"Addition: {a} + {b} = {addition}")
+
+subtraction = a - b
+print(f"Subtraction: {a} - {b} = {subtraction}")
+
+multiplication = a * b
+print(f"Multiplication: {a} * {b} = {multiplication}")
+
+if b != 0:
+    division = a / b
+    print(f"Division: {a} / {b} = {division}")
+else:
+    print("Division by zero is not allowed")
